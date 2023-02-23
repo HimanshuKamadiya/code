165de141a0d914a5ded165ac651cdf87e7f8c343
HimanshuKamadiya <himanshukamadiya@yahoo.com>
2023-02-23 17:34:48 +0530

    new

---
+++ strings_all_ques/que_53.py
@@ -0,0 +1,7 @@
+#Write a Python program to find the first repeated character in a given string.
+a=input('the string: ')
+count=0
+for char in a:
+    if a.count(char)==2:
+        print(char)
+        
