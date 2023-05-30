8578d420fa471a72cf006fadad0fd91950a3735e
Himanshu Kamadiya <123982410+HimanshuKamadiya@users.noreply.github.com>
2023-05-30 14:52:45 +0530

    new

---
+++ classes/pproeclass.py
@@ -0,0 +1,8 @@
+class node():
+    def __init__(self, data):
+        self.rightside=None
+        self.data=data
+        self.leftside=None
+class tree:
+    def __init__(self) -> None:
+        self.h=None
